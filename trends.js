e181569d46bacf783c5a82522dbea21f930d693f
jeff <jeff.etale@student.moringaschool.com>
2023-07-06 01:49:08 +0300

    Added event listeners to genre links. Smooth scrolling to the section corresponding to a clicked link is now enabled

---
+++ trends.js
@@ -0,0 +1,31 @@
+// Event listener for genre links
+document.addEventListener('DOMContentLoaded', function() {
+    // Get all genre links
+    const genreLinks = document.querySelectorAll('nav ul li a');
+    
+    // Add event listener to each genre link
+    genreLinks.forEach(function(link) {
+      link.addEventListener('click', function(event) {
+        event.preventDefault();
+        
+        // Get the target section ID from the link's href attribute
+        const targetSectionId = link.getAttribute('href').substring(1);
+        
+        // Scroll to the target section
+        scrollToSection(targetSectionId);
+      });
+    });
+    
+    // Function to scroll to a section
+    function scrollToSection(sectionId) {
+      const targetSection = document.getElementById(sectionId);
+      
+      if (targetSection) {
+        targetSection.scrollIntoView({
+          behavior: 'smooth'
+        });
+      }
+    }
+  });
+  
+
